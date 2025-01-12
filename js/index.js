44c93807fb257316e737e0e82a4cbe6b99afa7fe
Sierra <sierra.ar.sager@gmail.com>
2025-01-12 16:53:25 -0500

    completed assignment

--- js/index.js
+++ js/index.js
@@ -2,3 +2,22 @@
 // that are not declared
 "use strict"
 
+console.log(1 >= 3); //output: false
+
+console.log(2 != 7); //output: true
+
+console.log( 2 || 6 || 8); //output: 2
+
+console.log(2 && 6 && 8); //output: 8
+
+console.log(15 / 3); //output: 5
+
+console.log(15 / 6); //output: 2.5
+
+console.log(15 % 6); //output: 3
+
+console.log(15 % 3); //output: 0
+
+console.log(20 == 10 + "10") //output: false
+
+console.log(20 == 10 + 10); //output: true
